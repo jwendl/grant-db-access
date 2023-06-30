99c969db49a56ad9f452c54b6dfadef6fad26732
Justin Wendlandt <jwendl@hotmail.com>
2023-06-29 20:41:23 -0700

    Updating to add github actions

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,3 @@
-Push-Location .\src
-dotnet publish .\GrantDatabase.CommandLine\GrantDatabase.CommandLine\GrantDatabase.CommandLine.csproj -c Release -r ubuntu.22.04-x64 -p:PublishSingleFile=true --self-contained true
+Push-Location .\src\GrantDatabase.CommandLine
+dotnet publish .\GrantDatabase.CommandLine.csproj -c Release -r ubuntu.22.04-x64 -p:PublishSingleFile=true --self-contained true
 Pop-Location
