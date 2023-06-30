6322cfe5265151941a0ca74612a28ce2b2726b84
Justin Wendlandt <jwendl@hotmail.com>
2023-06-29 23:10:13 -0700

    Updating logging

--- src/GrantDatabase.CommandLine/Commands/AlterRoleCommand.cs
+++ src/GrantDatabase.CommandLine/Commands/AlterRoleCommand.cs
@@ -2,6 +2,7 @@
 using Azure.Identity;
 using GrantDatabase.CommandLine.Commands.Settings;
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
 using Spectre.Console.Cli;
 
 namespace GrantDatabase.CommandLine.Commands
@@ -9,15 +10,23 @@ namespace GrantDatabase.CommandLine.Commands
     public class AlterRoleCommand
         : AsyncCommand<AlterRoleSettings>
     {
+        private readonly ILogger<AlterRoleCommand> _logger;
+
+        public AlterRoleCommand(ILogger<AlterRoleCommand> logger)
+        {
+            _logger = logger;
+        }
+
         public override async Task<int> ExecuteAsync(CommandContext context, AlterRoleSettings settings)
         {
+            _logger.LogInformation("Running role command for {user} being added to {role}", settings.ManagedIdentityName, settings.RoleName);
+
             var defaultAzureCredential = new DefaultAzureCredential();
             var tokenRequestContext = new TokenRequestContext(new[] { "https://database.windows.net/.default" });
             var accessToken = await defaultAzureCredential.GetTokenAsync(tokenRequestContext);
 
             var connectionStringBuilder = new SqlConnectionStringBuilder
             {
-                Authentication = SqlAuthenticationMethod.ActiveDirectoryManagedIdentity,
                 DataSource = settings.SqlDatabaseName,
                 InitialCatalog = settings.SqlDatabaseName,
                 Encrypt = true
@@ -25,6 +34,7 @@ namespace GrantDatabase.CommandLine.Commands
 
             using var sqlConnection = new SqlConnection(connectionStringBuilder.ConnectionString);
             sqlConnection.AccessToken = accessToken.Token;
+            await sqlConnection.OpenAsync();
 
             using var sqlCommand = sqlConnection.CreateCommand();
             sqlCommand.CommandText = $"ALTER ROLE {settings.RoleName} ADD MEMBER [{settings.ManagedIdentityName}];";
--- src/GrantDatabase.CommandLine/Commands/CreateUserCommand.cs
+++ src/GrantDatabase.CommandLine/Commands/CreateUserCommand.cs
@@ -2,6 +2,7 @@
 using Azure.Identity;
 using GrantDatabase.CommandLine.Commands.Settings;
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
 using Spectre.Console.Cli;
 
 namespace GrantDatabase.CommandLine.Commands
@@ -9,15 +10,23 @@ namespace GrantDatabase.CommandLine.Commands
     public class CreateUserCommand
         : AsyncCommand<CreateUserSettings>
     {
+        private readonly ILogger<CreateUserCommand> _logger;
+
+        public CreateUserCommand(ILogger<CreateUserCommand> logger)
+        {
+            _logger = logger;
+        }
+
         public override async Task<int> ExecuteAsync(CommandContext context, CreateUserSettings settings)
         {
+            _logger.LogInformation("Running user command for {user}", settings.ManagedIdentityName);
+
             var defaultAzureCredential = new DefaultAzureCredential();
             var tokenRequestContext = new TokenRequestContext(new[] { "https://database.windows.net/.default" });
             var accessToken = await defaultAzureCredential.GetTokenAsync(tokenRequestContext);
 
             var connectionStringBuilder = new SqlConnectionStringBuilder
             {
-                Authentication = SqlAuthenticationMethod.ActiveDirectoryManagedIdentity,
                 DataSource = settings.SqlDatabaseName,
                 InitialCatalog = settings.SqlDatabaseName,
                 Encrypt = true
@@ -25,6 +34,7 @@ namespace GrantDatabase.CommandLine.Commands
 
             using var sqlConnection = new SqlConnection(connectionStringBuilder.ConnectionString);
             sqlConnection.AccessToken = accessToken.Token;
+            await sqlConnection.OpenAsync();
 
             using var sqlCommand = sqlConnection.CreateCommand();
             sqlCommand.CommandText = $"DROP USER IF EXISTS [{settings.ManagedIdentityName}]; CREATE USER [{settings.ManagedIdentityName}] FROM EXTERNAL PROVIDER;";
